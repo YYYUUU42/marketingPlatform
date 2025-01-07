0b38fa8e00375e64f220cd65ec905df43f87802b
yu <2758345412@qq.com>
2025-01-07 16:44:07 +0800

    fix：修复 Redisson 乱码问题

--- marketing-platform-main/src/main/java/com/yu/market/main/config/RedisClientConfig.java
+++ marketing-platform-main/src/main/java/com/yu/market/main/config/RedisClientConfig.java
@@ -1,26 +1,18 @@
 package com.yu.market.main.config;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.ByteBufOutputStream;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.client.codec.BaseCodec;
-import org.redisson.client.codec.StringCodec;
-import org.redisson.client.protocol.Decoder;
-import org.redisson.client.protocol.Encoder;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.IOException;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
 @Configuration
@@ -29,8 +21,10 @@ public class RedisClientConfig {
 
     @Bean("redissonClient")
     public RedissonClient redissonClient(ConfigurableApplicationContext applicationContext, RedisClientConfigProperties properties) {
+        log.info("redisson 配置");
+
         Config config = new Config();
-        config.setCodec(new RedisCodec());
+        config.setCodec(new JsonJacksonCodec());
 
         config.useSingleServer()
                 .setAddress("redis://" + properties.getHost() + ":" + properties.getPort())
@@ -48,35 +42,21 @@ public class RedisClientConfig {
         return Redisson.create(config);
     }
 
-    static class RedisCodec extends BaseCodec {
-
-        private final Encoder encoder = in -> {
-            ByteBuf out = ByteBufAllocator.DEFAULT.buffer();
-            try {
-                ByteBufOutputStream os = new ByteBufOutputStream(out);
-                JSON.writeJSONString(os, in, SerializerFeature.WriteClassName);
-                return os.buffer();
-            } catch (IOException e) {
-                out.release();
-                throw e;
-            } catch (Exception e) {
-                out.release();
-                throw new IOException(e);
-            }
-        };
-
-        private final Decoder<Object> decoder = (buf, state) -> JSON.parseObject(new ByteBufInputStream(buf), Object.class);
-
-        @Override
-        public Decoder<Object> getValueDecoder() {
-            return decoder;
-        }
-
-        @Override
-        public Encoder getValueEncoder() {
-            return encoder;
-        }
-
+    /**
+     * 配置 RedisTemplate Bean
+     *
+     * @param factory redis连接 factory
+     * @return redisTemplate
+     */
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
     }
-
 }
